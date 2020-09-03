820de6025c1f7529804a3f431b8e7fdd0e28073e
Marco De La Pierre <marco.delapierre@gmail.com>
2020-09-03 16:52:43 +0800

    added exercises for ep 1

---
+++ exercises/1.python/prepare4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# run this from the miniconda3 container
+# docker run --rm -it -v $(pwd):/data -w /data python:3.8-slim bash
+
+pip install -r requirements.in
+
+REQ_LABEL="3sep"
+REQ_FILE="requirements4-${REQ_LABEL}.txt"
+pip freeze >$REQ_FILE
